84be3f006337db05191767ed51ad33d97128303b
Burak5796 <burak.d5796@gmail.com>
2023-01-23 15:46:59 +0100

    react

---
+++ myapp/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { BrowserRouter, Routes, Router } from 'react-router-dom';
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Router/>
+    </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
